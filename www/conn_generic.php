9ded733d9730eac591636fce24a59ec22b17d675
MallocNull <aleco@aroltd.com>
2014-07-09 17:00:12 -0500

    bob ross
    i hate my life

--- www/conn_generic.php
+++ www/conn_generic.php
@@ -2,4 +2,13 @@
 mysql_connect("HOSTADDRESS", "USERNAME", "PASSWORD");
 mysql_select_db("DATABASE_NAME");
 session_start();
+
+$config = mysql_fetch_object(mysql_query("SELECT * FROM `config` WHERE `id`=1"));
+
+function checkIfLoggedIn() {
+    if(mysql_num_rows(mysql_query("SELECT * FROM `admin` WHERE `username`='". mysql_real_escape_string($_SESSION['user']) ."' AND `password`='". $_SESSION['pwd'] ."'")) > 0)
+        return true;
+    else
+        return false;
+}
 ?>
